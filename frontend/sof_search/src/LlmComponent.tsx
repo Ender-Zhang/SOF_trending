a490fe6e3a843cdf65d6a65fd169e870807f1e52
Ender-Zhang <102596313+Ender-Zhang@users.noreply.github.com>
2023-12-11 15:37:29 -0500

    final version

--- frontend/sof_search/src/LlmComponent.tsx
+++ frontend/sof_search/src/LlmComponent.tsx
@@ -12,16 +12,26 @@ const LlmComponent: React.FC = () => {
     } as RequestInit;
 
     fetch("http://127.0.0.1:8000/llm", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        if (result.image && result.text) {
-          setImageSrc(`data:image/png;base64,${result.image}`);
-          setTextList(result.text);
-        }
-      })
-      .catch((error) => console.log("error", error));
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result.image && result.text) {
+        setImageSrc(`data:image/png;base64,${result.image}`);
+  
+        // 更新正则表达式以匹配提供的格式
+        const updatedResults1 = result.text.map((item:any) => {
+          const pattern = /(\d+)_([a-zA-Z0-9-]+)/;
+          const replacement = "https://stackoverflow.com/questions/$1/$2.html";
+          console.log(item.split(" ")[1].replace(pattern, replacement));
+          return item.split(" ")[1].replace(pattern, replacement);
+        });
+        console.log(updatedResults1);
+        setTextList(updatedResults1);
+      }
+    })
+    .catch((error) => console.log("error", error));
   }, []); // 空依赖数组意味着这个effect只会在组件挂载时运行一次
+  
 
   return (
     <div >
@@ -32,12 +42,14 @@ const LlmComponent: React.FC = () => {
             const parts = text.split(" ");
             return (
               <li key={index}>
-                {parts[0]}{" "}
+                {/* {parts[0]}{" "}
                 {parts[1] && (
                   <a href={parts[1]} target="_blank" rel="noopener noreferrer">
                     {parts[1]}
                   </a>
-                )}
+                )
+                } */}
+                <a href={text} target="_blank" rel="noopener noreferrer">{parts}</a>
               </li>
             );
           })}
